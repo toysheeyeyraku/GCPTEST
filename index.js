55c8ed3e1698d7de858128c961ff569f1f7871e1
Bohdan Kovtun <kirpich1337228@gmail.com>
2021-04-15 14:01:16 +0300

    test

---
+++ index.js
@@ -0,0 +1,11 @@
+var express = require('express')
+
+var app = express();
+
+app.get('/', (req, res)=> {
+    res.send("HI FROM JS")
+})
+
+app.listen(3000, () =>{
+    console.log('Server is up')
+});
